cde3520c7c1040f89eae0173dea6d605dd05f56d
Bhoomika <150054642+Bhoomikahk@users.noreply.github.com>
2025-06-11 21:26:39 +0530

    Add files via upload

---
+++ Lab001_java.java
@@ -0,0 +1,22 @@
+package ex_01_java_basic;
+
+public class Lab001_java {
+    public static void main(String[] args) {
+        int a = 10;
+        int b=20;
+        //printf
+       System.out.printf("your variable name is %d", a);
+       /* %d - int,byte,long,short
+        %s - string
+        % f - float,double
+        %b - boolean*/
+        System.out.printf("%d * %d" ,a ,b );
+        //final
+        final float pi=3.14f;
+        char c11='\u1F60';
+        char my_laugh_smily='\u1F60';
+        System.out.println(my_laugh_smily);
+
+
+    }
+}
